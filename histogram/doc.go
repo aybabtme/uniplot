db8675141adc54eb0c719a2daf3c0c4108a1c10b
Antoine Grondin <antoinegrondin@gmail.com>
2014-06-03 16:46:43 -0400

    Move histogram to subpackage.

--- histogram/doc.go
+++ histogram/doc.go
@@ -3,6 +3,6 @@ Package histogram is a helper to quickly create histograms from slices
 of float64.
 
 You can use it with any numeric value, given that you convert it back
-and forth to []float64:
+and forth to []float64.
 */
 package histogram
