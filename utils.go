7727acc5900877021e6cdf7a86270f4f161f188a
Antoine Grondin <antoinegrondin@gmail.com>
2014-06-03 15:43:14 -0400

    Fix bug in barcharting.

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"strconv"
 	"strings"
 	"text/tabwriter"
 )
@@ -16,10 +17,11 @@ var blocks = []string{
 	"▏", "▎", "▍", "▌", "▋", "▊", "▉", "█",
 }
 
-var barchar = func(v float64) string {
-	digit := (v - math.Floor(v)) * 10.0
-	diff := int(10.0 * digit / 8.0)
-	return blocks[diff]
+var barstring = func(v float64) string {
+	decimalf := (v - math.Floor(v)) * 10.0
+	decimali := math.Floor(decimalf)
+	charIdx := int(decimali / 10.0 * 8.0)
+	return strings.Repeat("█", int(v)) + blocks[charIdx]
 }
 
 // Fprint prints a unicode histogram on the io.Writer, using
@@ -30,15 +32,15 @@ var barchar = func(v float64) string {
 //
 // ... yields the graph:
 //
-//	0.1-0.2  5%   ▉       [1/20]
-//	0.2-0.3  25%  ██▉     [5/20]
-//	0.3-0.4  0%   ▏       [0/20]
-//	0.4-0.5  5%   ▉       [1/20]
-//	0.5-0.6  50%  █████▏  [10/20]
-//	0.6-0.7  0%   ▏       [0/20]
-//	0.7-0.8  0%   ▏       [0/20]
-//	0.8-0.9  5%   ▉       [1/20]
-//	0.9-1    10%  █▏      [2/20]
+//	0.1-0.2  5%   ▋1
+//	0.2-0.3  25%  ██▋5
+//	0.3-0.4  0%   ▏
+//	0.4-0.5  5%   ▋1
+//	0.5-0.6  50%  █████▏10
+//	0.6-0.7  0%   ▏
+//	0.7-0.8  0%   ▏
+//	0.8-0.9  5%   ▋1
+//	0.9-1    10%  █▏2
 func Fprint(w io.Writer, h Histogram, s ScaleFunc) error {
 	return fprintf(w, h, s, func(v float64) string {
 		return fmt.Sprintf("%.4g", v)
@@ -52,14 +54,20 @@ func Fprintf(w io.Writer, h Histogram, s ScaleFunc, f FormatFunc) error {
 
 func fprintf(w io.Writer, h Histogram, s ScaleFunc, f FormatFunc) error {
 	tabw := tabwriter.NewWriter(w, 2, 2, 2, byte(' '), 0)
+
+	yfmt := func(y int) string {
+		if y > 0 {
+			return strconv.Itoa(y)
+		}
+		return ""
+	}
+
 	for i, bkt := range h.Buckets {
 		sz := h.Scale(s, i)
-		fmt.Fprintf(tabw, "%s-%s\t%.3g%%\t%s\t[%d/%d]\n",
+		fmt.Fprintf(tabw, "%s-%s\t%.3g%%\t%s\n",
 			f(bkt.Min), f(bkt.Max),
 			float64(bkt.Count)*100.0/float64(h.Count),
-			strings.Repeat("█", int(sz))+barchar(sz),
-			bkt.Count,
-			h.Count,
+			barstring(sz)+yfmt(bkt.Count),
 		)
 	}
 
