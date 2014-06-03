a9e283d6de480e5636dd70eb146481deced016db
Antoine Grondin <antoinegrondin@gmail.com>
2014-06-03 13:14:34 -0400

    Add FormatFunc + FPrintf

--- histogram_test.go
+++ histogram_test.go
@@ -2,6 +2,7 @@ package histogram
 
 import (
 	"os"
+	"time"
 )
 
 func ExampleHist() {
@@ -33,4 +34,51 @@ func ExampleHist() {
 	// 0.7-0.8  0%   ▏       [0/20]
 	// 0.8-0.9  5%   ▉       [1/20]
 	// 0.9-1    10%  █▏      [2/20]
+}
+
+func ExampleHist_duration() {
+	data := []float64{
+		float64(time.Millisecond * 100),
+		float64(time.Millisecond * 200),
+		float64(time.Millisecond * 210),
+		float64(time.Millisecond * 220),
+		float64(time.Millisecond * 221),
+		float64(time.Millisecond * 222),
+		float64(time.Millisecond * 223),
+		float64(time.Millisecond * 300),
+		float64(time.Millisecond * 400),
+		float64(time.Millisecond * 500),
+		float64(time.Millisecond * 510),
+		float64(time.Millisecond * 520),
+		float64(time.Millisecond * 530),
+		float64(time.Millisecond * 540),
+		float64(time.Millisecond * 550),
+		float64(time.Millisecond * 560),
+		float64(time.Millisecond * 570),
+		float64(time.Millisecond * 580),
+		float64(time.Millisecond * 600),
+		// no 0.7s
+		float64(time.Millisecond * 800),
+		float64(time.Millisecond * 900),
+		float64(time.Millisecond * 1000),
+	}
+
+	hist := Hist(9, data)
+
+	err := FPrintf(os.Stdout, hist, Linear(5), func(v float64) string {
+		return time.Duration(v).String()
+	})
+	if err != nil {
+		panic(err)
+	}
+	// Output:
+	// 100ms-200ms  4.55%  ▉       [1/22]
+	// 200ms-300ms  27.3%  ███▋    [6/22]
+	// 300ms-400ms  4.55%  ▉       [1/22]
+	// 400ms-500ms  4.55%  ▉       [1/22]
+	// 500ms-600ms  40.9%  █████▏  [9/22]
+	// 600ms-700ms  4.55%  ▉       [1/22]
+	// 700ms-800ms  0%     ▏       [0/22]
+	// 800ms-900ms  4.55%  ▉       [1/22]
+	// 900ms-1s     9.09%  █▎      [2/22]
 }
--- utils.go
+++ utils.go
@@ -8,6 +8,10 @@ import (
 	"text/tabwriter"
 )
 
+// FormatFunc formats a float into the proper string form. Used to
+// print meaningful axe labels.
+type FormatFunc func(v float64) string
+
 var blocks = []string{
 	"▏", "▎", "▍", "▌", "▋", "▊", "▉", "█",
 }
@@ -36,11 +40,22 @@ var barchar = func(v float64) string {
 //	0.8-0.9  5%   ▉       [1/20]
 //	0.9-1    10%  █▏      [2/20]
 func FPrint(w io.Writer, h Histogram, s ScaleFunc) error {
+	return fprintf(w, h, s, func(v float64) string {
+		return fmt.Sprintf("%.4g", v)
+	})
+}
+
+// FPrintf is the same as FPrint, but applies f to the axis labels.
+func FPrintf(w io.Writer, h Histogram, s ScaleFunc, f FormatFunc) error {
+	return fprintf(w, h, s, f)
+}
+
+func fprintf(w io.Writer, h Histogram, s ScaleFunc, f FormatFunc) error {
 	tabw := tabwriter.NewWriter(w, 2, 2, 2, byte(' '), 0)
 	for i, bkt := range h.Buckets {
 		sz := h.Scale(s, i)
-		fmt.Fprintf(tabw, "%.4g-%.4g\t%.3g%%\t%s\t[%d/%d]\n",
-			bkt.Min, bkt.Max,
+		fmt.Fprintf(tabw, "%s-%s\t%.3g%%\t%s\t[%d/%d]\n",
+			f(bkt.Min), f(bkt.Max),
 			float64(bkt.Count)*100.0/float64(h.Count),
 			strings.Repeat("█", int(sz))+barchar(sz),
 			bkt.Count,
